a839fbb011c2aa5dfd68ee43cf930be03fcdc42e
grysj <jgrys17@gmail.com>
2024-09-05 02:35:37 +0200

    add some tests

---
+++ db/sqlc/list_test.go
@@ -0,0 +1,72 @@
+package db
+
+import (
+	"backend/util"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func generateRandomList() (List, error) {
+	randUser, _ := generateRandomUser()
+	randTitle := util.RandomTitle()
+	listParams := CreateListParams{
+		CreatedBy: randUser.UserID,
+		Title:     randTitle,
+	}
+	list, err := testQueries.CreateList(context.Background(), listParams)
+	return list, err
+}
+
+func TestCreateList(t *testing.T) {
+	randUser, _ := generateRandomUser()
+	randTitle := util.RandomTitle()
+	listParams := CreateListParams{
+		CreatedBy: randUser.UserID,
+		Title:     randTitle,
+	}
+	list, err := testQueries.CreateList(context.Background(), listParams)
+	require.NoError(t, err)
+	require.NotEmpty(t, list)
+
+	require.Equal(t, randUser.UserID, list.CreatedBy)
+	require.Equal(t, randTitle, list.Title)
+
+	require.NotEmpty(t, list.ListID)
+	require.NotEmpty(t, list.CreatedAt)
+}
+
+func TestEditTitle(t *testing.T) {
+	list, _ := generateRandomList()
+
+	newTitle := util.RandomTitle()
+
+	editParams := EditTileParams{
+		ListID: list.ListID,
+		Title:  newTitle,
+	}
+
+	err := testQueries.EditTile(context.Background(), editParams)
+	require.NoError(t, err)
+
+	newList, _ := testQueries.GetList(context.Background(), list.ListID)
+
+	require.Equal(t, list.CreatedAt, newList.CreatedAt)
+	require.Equal(t, list.CreatedBy, newList.CreatedBy)
+	require.Equal(t, newTitle, newList.Title)
+
+}
+
+func TestGetList(t *testing.T) {
+	list, _ := generateRandomList()
+
+	foundList, err := testQueries.GetList(context.Background(), list.ListID)
+	require.NoError(t, err)
+	require.NotEmpty(t, foundList)
+
+	require.Equal(t, list.CreatedAt, foundList.CreatedAt)
+	require.Equal(t, list.CreatedBy, foundList.CreatedBy)
+	require.Equal(t, list.ListID, foundList.ListID)
+	require.Equal(t, list.Title, foundList.Title)
+}
--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -8,9 +8,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T) {
-	arg := util.RandomString(7)
+func generateRandomUser() (User, error) {
+	arg := util.RandomUser()
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	return user, err
+}
 
+func TestCreateUser(t *testing.T) {
+
+	arg := util.RandomUser()
 	user, err := testQueries.CreateUser(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
@@ -22,11 +28,29 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestGetUser(t *testing.T) {
-	randUser, err := testQueries.CreateUser(context.Background(), util.RandomUser())
-	require.NoError(t, err)
-	user, err := testQueries.GetUser(context.Background(), randUser.UserID)
-	require.NoError(t, err)
+	randUser, err1 := generateRandomUser()
+	require.NoError(t, err1)
+	user, err2 := testQueries.GetUser(context.Background(), randUser.UserID)
+	require.NoError(t, err2)
 	require.Equal(t, randUser.UserID, user.UserID)
 	require.Equal(t, randUser.Username, user.Username)
 	require.Equal(t, randUser.CreatedAt, user.CreatedAt)
+}
+
+func TestCountUser(t *testing.T) {
+	countBefore, err1 := testQueries.CountUsers(context.Background())
+	require.NoError(t, err1)
+	require.NotEmpty(t, countBefore)
+	n := util.RandomInt(1, 64)
+
+	var i int64
+	for i = 0; i < n; i++ {
+		generateRandomUser()
+	}
+
+	countAfter, err2 := testQueries.CountUsers(context.Background())
+	require.NoError(t, err2)
+	require.NotEmpty(t, countAfter)
+	require.Equal(t, n, countAfter-countBefore)
+
 }
